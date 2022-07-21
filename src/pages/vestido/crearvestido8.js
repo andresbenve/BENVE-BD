a5a4ab9a851b56ffb3fafa8c7e19575a1363b0f3
ANDRÉS BENVENUTO <56841920+andresbenve@users.noreply.github.com>
2022-07-21 16:52:11 -0300

    ahora si

--- src/pages/vestido/crearvestido8.js
+++ src/pages/vestido/crearvestido8.js
@@ -32,7 +32,7 @@ function validate(input) {
 const CreateDress = () => {
   const createnewDress = async () => {
     try {
-      await fetch("http://localhost:3000/api/vestidos/vestido8", {
+      await fetch("https://benve-bd.vercel.app/api/vestidos/vestido8", {
         method: "POST",
         body: JSON.stringify(input),
         headers: {
